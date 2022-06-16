9404d384e16db102070b65310b23a0bb16953395
kurosaki-kun0 <107650350+kurosaki-kun0@users.noreply.github.com>
2022-06-16 23:11:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+212 644-424696']
+global.premium = ['+212 644-424696']
+global.ownernomer = 'kurosaki-kun'
+global.ownername = '🐦Yaya'
+global.botname = 'kurosaki-kun'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
+global.sc = 'https://github.com/Yahyakurosaki/Kurosaki-kun'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'kurosaki-kun'
+global.author = '🐦'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
